ac60efc1dce3a285557ffc1f45e135453094eccc
Paulo Cardoso <paulo_jcs@yahoo.com.br>
2020-06-24 14:38:46 -0300

    Exercicio react-native

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Image} from 'react-native';
+import Estilo from './src/estilo/padrao';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class RevistaCactos extends Component{
+  render(){
+    const textoPrincipal = 'Este livro conta a historia um empreendimento de sucesso, a CACTOS DEV. Que inicio no dia 17/06/2020. Dois anos depois, se tornou uma das maiores empresas de tecnologia do pais, desenvolvendos APP, Jogos e Sites para as maiores empresas do Mercado.';
+    return(
+      <View style= {Estilo.estilo1}>
+        <View style = {Estilo.view1}>
+          <View style={Estilo.view1_1}>
+          </View>
+
+          <View style={Estilo.view1_2}>
+          </View>
+
+          <View style={Estilo.view1_3}>
+          </View>
+
+          <View style={Estilo.view1_4}>
+          </View>
+        </View>
+
+        <View style = {Estilo.view2}>
+          <Text style= {Estilo.view2_font}> Revista Cactos Dev </Text>
+        </View>
+        <View style = {Estilo.view3}>
+          <View style = {Estilo.view3_1}>
+
+            <Text style= {Estilo.view3_font}>
+              {textoPrincipal.slice(0,250)}
+            </Text>
+           
+          </View>
+
+          <View style = {Estilo.view3_2}>
+            <Image 
+              style ={Estilo.view3_image}
+              source={require('./src/imagens/cactos.png')} 
+              
+            />
+           
+          </View>
+        </View>
+
+        <View style = {Estilo.view4}>
+        </View >
+
+        <View style = {Estilo.view5}>
+          <Text style = {Estilo.view5_font}> Copyright (c) Cactos Dev </Text>
+        </View>
+        
+
+        
+
+      </View>
+
+    );
+  }
 }
+ export default RevistaCactos;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
---
+++ src/estilo/padrao.js
@@ -0,0 +1,84 @@
+import {StyleSheet, StatusBar} from 'react-native'
+export default StyleSheet.create({
+	estilo1: {
+        marginTop: 20,   
+        flex: 1,
+        backgroundColor: "orange", 
+        padding:8,
+    },
+    
+    view1: {
+        flex: 1,
+        flexDirection: 'row', 
+    },
+    view1_1: {
+        flex: 1,
+        backgroundColor: '#832a0d',
+    },
+    view1_2: {
+        flex: 1,
+        backgroundColor: '#A0522D',
+    },
+    view1_3: {
+        flex: 1,
+        backgroundColor: '#F4A460',
+    },
+    view1_4: {
+        flex: 1,
+        backgroundColor: '#BDB76B',
+    },
+    
+    view2: {
+        flex: 1,
+        backgroundColor: '#FFDEAD', 
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 7,       
+    },
+    view2_font: {
+        color: '#000000',
+        fontSize: 22,   
+    },
+
+    view3: {
+        flex: 6,
+        backgroundColor: '#FFDEAD',
+        borderBottomWidth: 7,
+        flexDirection: 'row', 
+ 
+    },
+    view3_1: {
+        flex: 6,
+    },
+    view3_2: {
+        flex: 4,
+        borderWidth: 2,  
+        padding: 4, 
+    },
+    view3_font: {
+        fontSize: 17,          
+    },
+    view3_image: {
+        flex: 1,
+        width: undefined,
+        height: undefined,    
+           
+    },
+
+    view4: {
+        flex: 7,
+        backgroundColor: '#FFDEAD',
+        borderBottomWidth: 7,
+    },
+
+    view5: {
+        flex: 1,
+        backgroundColor: '#FFFFF0',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    view5_font: {
+        fontSize: 15,   
+    },
+
+})
